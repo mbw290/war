b33524d713fc550630dc43a721e88d5405b67b8d
Matt <anonym0us@localhost.localdomain>
2014-03-08 12:22:18 -0500

    populate deck

--- war.cpp
+++ war.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
-class Card
+class Cards
 {
-int value;
+public:
+  int value;
 };
 
 class Deck
+{
+public:
+  Cards wDeck[52];
+//Populate the deck with cards
+  void fillDeck()
+  {
+  int i =0;
+  int cardnum=2;
+  while (i<52)
+  {
+      for (int j=1;j<5;j++)
+      {
+        wDeck[i].value=cardnum;
+        i++;
+      }
+    cardnum++;
+    }
+  }
+//Print method for debugging
+  void printDeck()
+  {
+    for(int i=0; i<52; i++)
+    {
+      cout << wDeck[i].value << "\n";
+    }
+  }
+};
+
+int main()
+{
+Deck nDeck;
+nDeck.fillDeck();
+nDeck.printDeck();
+return 0;
+}
